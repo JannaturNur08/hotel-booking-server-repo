d787fb519720ca5628905b1209098f835bf2d42e
JannaturNur08 <nur@playense.com>
2023-11-05 23:54:50 +0600

    jwt authentication completed

--- index.js
+++ index.js
@@ -28,6 +28,36 @@ const client = new MongoClient(uri, {
 	},
 });
 
+//middlewares
+const logger = async (req, res, next) => {
+	console.log("called", req.host);
+	next();
+}
+
+
+//verify token and grant access
+
+const verifyToken = async (req, res, next) => {
+	const token = req.cookies?.token;
+	console.log("value of token in middleware", token);
+	if (!token) {
+		return res.status(401).send({ message: "not authorized" });
+	}
+	// verify a token symmetric
+	jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+		//error
+		if (err) {
+			console.log(err);
+			return res.status(401).send({ message: "unauthorized" });
+		}
+		//if token is valid it would be decoded
+		console.log("value in the token", decoded)
+		//attach decoded user so that others can get it
+		req.user=decoded;
+		next();
+	});
+};
+
 async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
@@ -36,7 +66,7 @@ async function run() {
 		const roomCollection = client.db("hotelBook").collection("rooms");
 
 		//jwt
-		app.post("/jwt", async (req, res) => {
+		app.post("/jwt",logger, async (req, res) => {
 			//send to client
 			const user = req.body;
 			const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
@@ -47,6 +77,11 @@ async function run() {
 				secure: false,
 			}).send({ success: true });
 		});
+		app.post('/logout', async(req,res)=> {
+			const user = req.body;
+			console.log('logging out',user);
+			res.clearCookie('token',{maxAge: 0}).send({success:true})
+		})
 
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
